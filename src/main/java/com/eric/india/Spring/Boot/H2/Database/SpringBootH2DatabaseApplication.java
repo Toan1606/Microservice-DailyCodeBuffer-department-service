aed176a9da1f0a30bcd37197acc3a4e945916d52
codehunter <nguyenthetoan1606@gmail.com>
2022-09-13 22:05:57 +0700

    add

--- src/main/java/com/eric/india/Spring/Boot/H2/Database/SpringBootH2DatabaseApplication.java
+++ src/main/java/com/eric/india/Spring/Boot/H2/Database/SpringBootH2DatabaseApplication.java
@@ -2,8 +2,10 @@ package com.eric.india.Spring.Boot.H2.Database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootH2DatabaseApplication {
 
 	public static void main(String[] args) {
